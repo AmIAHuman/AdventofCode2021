18bd1ea666db2fa0955e5723c19307e8ade8bdf5
AmIAHuman <30362241+AmIAHuman@users.noreply.github.com>
2021-12-02 23:30:20 -0800

    Update template.java

--- template.java
+++ template.java
@@ -3,6 +3,18 @@ import java.io.File;
 
 public class Template {
   public static void main(String[] args) {
+    System.out.println("Part 1 answer: " + Part1());
+    System.out.println("Part 2 answer: " + Part2());
+  }
+  public static void Part1() {
+    try {
+      Scanner scanner = new Scanner(new File("input.txt"));
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+      }
+    } catch(Exception e){}
+  }
+  public static void Part2()) {
     try {
       Scanner scanner = new Scanner(new File("input.txt"));
       while (scanner.hasNextLine()) {
